5830ac7554fbdb87d30b5b4eff6c0ab944920671
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-12-12 19:06:14 +0200

    Added examples for accessing config properties programmatically and via annotations

--- src/main/java/com/adisava/ApplicationRoutes.java
+++ src/main/java/com/adisava/ApplicationRoutes.java
@@ -1,10 +1,12 @@
 package com.adisava;
 
+import io.quarkus.runtime.configuration.ProfileManager;
 import io.quarkus.vertx.http.runtime.filters.Filters;
 import io.quarkus.vertx.web.Route;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -12,12 +14,28 @@ import javax.enterprise.event.Observes;
 @ApplicationScoped
 public class ApplicationRoutes {
 
+    String currentProfile = ProfileManager.getActiveProfile();
+
+    /*
+    Manually retrieve profile-bound application
+    properties; you don’t need to qualify the property
+    names with the %profile prefix
+     */
+    String appName = ConfigProvider.getConfig().getValue("quarkus.application.name", String.class);
+
     public void routes(@Observes Router router) {
         /*
         Reactive route, command: curl http://localhost:8080/ok
          */
 
-        router.get("/ok").handler(routingContext -> routingContext.response().end("OK from Route"));
+        router.get("/ok")
+                .handler(routingContext -> routingContext.response().end("OK from Route"));
+
+        router.get("/profile")
+                .handler(routingContext -> routingContext.response().end("Current profile is " + currentProfile));
+
+        router.get("/app-name")
+                .handler(routingContext -> routingContext.response().end("Application name is " + appName));
     }
 
     /*
