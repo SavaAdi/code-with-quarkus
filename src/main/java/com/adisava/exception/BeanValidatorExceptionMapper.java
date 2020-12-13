ad42d89ec211c4c05c922f4a459a0ac59bb8da32
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-12-13 15:34:31 +0200

    Added ValidationException mapper with json-b implementation

---
+++ src/main/java/com/adisava/exception/BeanValidatorExceptionMapper.java
@@ -0,0 +1,36 @@
+package com.adisava.exception;
+
+import javax.json.Json;
+
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class BeanValidatorExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+
+    @Override
+    public Response toResponse(ConstraintViolationException exception) {
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(createErrorMessage(exception))
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    private JsonArray createErrorMessage(ConstraintViolationException exception) {
+        JsonArrayBuilder errors = Json.createArrayBuilder();
+        for (ConstraintViolation<?> violation : exception.getConstraintViolations()) {
+            errors.add(
+                    Json.createObjectBuilder()
+                            .add("path", violation.getPropertyPath().toString())
+                            .add("message", violation.getMessage())
+            );
+        }
+        return errors.build();
+    }
+}
