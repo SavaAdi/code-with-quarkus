599432ed479631ca53565501907417d3f033a667
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-12-15 21:33:59 +0200

    Added behavioral test example

--- src/test/java/com/adisava/GreetingResourceTest.java
+++ src/test/java/com/adisava/GreetingResourceTest.java
@@ -4,18 +4,24 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
+        String authorization = "xyz";
+        GreetingResource.Order order = GreetingResource.Order.asc;
+
         given()
-          .when().get("/hello-resteasy")
+          .when()
+                .header("authorization", authorization)
+                .queryParam("order", order.toString())
+                .get("/hello-resteasy")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body(containsString("Order " + order.toString() + " - Authorization: " + authorization));
     }
 
 }
