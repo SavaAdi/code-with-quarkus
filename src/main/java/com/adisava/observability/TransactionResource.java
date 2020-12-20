367b31042f5502fe51bdd3c4886d1f494a2e24b3
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-12-20 22:10:39 +0200

    Added more metrics examples

--- src/main/java/com/adisava/observability/TransactionResource.java
+++ src/main/java/com/adisava/observability/TransactionResource.java
@@ -1,6 +1,8 @@
 package com.adisava.observability;
 
-import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.Histogram;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.*;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -11,12 +13,42 @@ import javax.ws.rs.core.Response;
 @Path("/tx")
 public class TransactionResource {
 
-    @Counted(name = "number-of-transactions",
+    @Metric(name = "transaction-evolution")
+    Histogram transactionHistogram;
+
+    private long highestTransaction = 0;
+
+    @Counted(
+            name = "number-of-transactions",
             displayName = "Transactions",
             description = "How many transactions have been processed")
+    @Metered(
+            name = "transactions",
+            unit = MetricUnits.SECONDS,
+            description = "Rate of transactions"
+    )
+    @Timed(
+            name = "average-transaction",
+            unit = MetricUnits.SECONDS,
+            description = "Average duration of transaction"
+    )
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response doTransaction(Transaction transaction) {
+        if (transaction.amount > highestTransaction) {
+            highestTransaction = transaction.amount;
+        }
+        transactionHistogram.update(transaction.amount);
         return Response.ok().build();
     }
+
+    @Gauge(
+            name = "highest-gross-transaction",
+            description = "Highest transaction so far.",
+            unit = MetricUnits.NONE
+    )
+    public long highestTransaction() {
+        return highestTransaction;
+    }
+
 }
